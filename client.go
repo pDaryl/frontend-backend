933c4bfc46f714b451817e3bb2afa7e0c7ecaa11
Daryl Peterson <pdaryl8@gmail.com>
2025-02-08 08:00:40 -0600

    set up gin

---
+++ client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, Gin!"})
+	})
+
+	r.Run(":8080") // Run server on port 8080
+}
